853891dd96cf9ab230ae9e76d57e3b160929d022
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-03-26 18:08:25 +0530

    rds,redis,rabbitmq,elasticache

---
+++ vars.tf
@@ -0,0 +1,8 @@
+variable "component" {}
+variable "env" {}
+variable "tags" {}
+variable "instance_type" {}
+variable "desired_capacity" {}
+variable "max_size" {}
+variable "min_size" {}
+variable "subnets" {}
